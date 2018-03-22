49e6b7f5eb6a4f6b13db90ca3a613b631bd9478a
Jon Szeto <jonszeto821@gmail.com>
2018-03-21 21:16:09 -0400

    Begin CSS styling for formation and profile page

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
 	console.log("loading client-side js");
 	console.log(dots);
-	$('button#save-btn').on('click', function() {
-		console.log('clicked!!!');
+	$('button.js-save-btn').on('click', function() {
+		console.log('Formation Saved!');
 		let newPosition = {'dots':JSON.stringify(dots)}
 
 		$.post('', newPosition, function() {
@@ -10,7 +10,27 @@ $(document).ready(function() {
 
 		});
 	});
+
+	//Cancel button
+	$('button.js-cancel-btn').on('click', function() {
+		console.log('Formation Cancelled!');
+		//redirect the user back to list of formations
+	});
+
+	//Fork button
+	/*create a new formation for the user of the current formation*/
+	$('#js-fork-btn').on('click', function() {
+		console.log('Formation Forked!');
+		//redirect the user back to list of formations
+	});
+
+	//Cancel button
+	$('button.js-delete-btn').on('click', function() {
+		console.log('Formation Deleted!');
+		//redirect the user back to list of formations
+	});
 });
+
 
 
 
@@ -21,4 +41,5 @@ function updateDots(d) {
 		}
 	}
 };
+
 
